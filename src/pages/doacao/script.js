d80fa9600e08a427e448db9ad218e0211031f3e1
nilton99 <niltonalmeida1999#gmail.com>
2025-06-18 22:37:42 -0300

    feat(modal)modal header

--- src/pages/doacao/script.js
+++ src/pages/doacao/script.js
@@ -1,4 +1,11 @@
-function abrirModal() {
-  const modal = document.getElementById("open-modal");
-  modal.classList.add('abrir');
-}
+const openModalButton = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const fade = document.getElementById('fade');
+
+const toggleModal = () => {
+  modal.classList.toggle('hide');
+  fade.classList.toggle('hide');
+};
+
+openModalButton.addEventListener('click', toggleModal);
+fade.addEventListener('click', toggleModal);

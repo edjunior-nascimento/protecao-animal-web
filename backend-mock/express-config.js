a98723a569f34ebcca39867a8ccc7cb38d847eaf
Edjunior Nascimento <edjr.programmer@gmail.com>
2025-07-14 22:38:13 -0300

    fix: porta express

--- backend-mock/express-config.js
+++ backend-mock/express-config.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json()); // para aceitar JSON no body

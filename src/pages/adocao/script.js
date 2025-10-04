de491b69efa79adbd548cfa24f9116e52cd97aa3
edjofra <edjofra123@hotmail.com>
2025-10-03 22:54:15 -0300

    feat(adocao): listagem dos animais do back  via axios

--- src/pages/adocao/script.js
+++ src/pages/adocao/script.js
@@ -1,33 +1,60 @@
-import { animals } from "./data.animals.js"
- 
 
-function listarAnimalsGallery(){
+let animaisGlobal = [] // variável global
+let indice = 0
+let tamanho = 3
+listarAnimaisAxios()
+
+function listarAnimaisAxios() {
+    axios.get("http://localhost:3001/api/animais")
+        .then(response => {
+            animaisGlobal = response.data.data // agora atualiza a variável global corretamente
+             
+            listarAnimals() // chama sem argumentos, pois usará a variável global
+        })
+        .catch(error => {
+            console.error("Falha ao carregar json:", error)
+        })
+}
+function listarAnimals(){
     
-        var galeria = document.querySelector(".galery")
+    let init = indice
+    let fim = indice + tamanho
+    let animaisVisiveis = animaisGlobal.slice(init, fim)
+        
+    var pets = document.querySelector(".galery")
+    pets.innerHTML = ""
 
-        function criarCards(card){
-
-            galeria.innerHTML+=`
+    animaisVisiveis.forEach(card => {
+    pets.innerHTML+=`
             <div class="card-animals">
-                <img src="../../assets/images/${card.imagem}" alt="animal-photo" class="img-animal">
+                <img src="${card.fotos[0]}" alt="animal-photo" class="img-animal">
                 <p class="card-name">${card.nome}</p>
                 <p class="card-local">${card.local}</p>
-            </div>
-        
+            </div>  
         `
-        }
+        })
 
-        function mostrarCards(){
-            for(let k = 0; k < animals.length; k++){
-                var animal = animals[k]
-                criarCards(animal)
-            }
-        }
-        mostrarCards()
+       
     }
     
-listarAnimalsGallery()
 
+function botaoAfter() {
+    if (indice + tamanho < animaisGlobal.length) {
+        indice += tamanho
+        listarAnimals()
+    }
+}
+
+
+function botaoPrev() {
+    if (indice - tamanho >= 0) {
+        indice -= tamanho
+        listarAnimals()
+    }
+}
+
+
+   
 /*MODAL*/
 const openModal = document.querySelector('.mobile-menu')
 const modal = document.querySelector('#modal')

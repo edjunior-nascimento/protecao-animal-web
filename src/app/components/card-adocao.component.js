e07f5252b586c26cd9b9d9b539859a6e7c72906d
edjofra <edjofra123@hotmail.com>
2026-02-09 23:38:54 -0300

    feat(adocao): componentizacao do card-adocao

--- src/app/components/card-adocao.component.js
+++ src/app/components/card-adocao.component.js
@@ -1,75 +1,113 @@
-export class CardAdocaoComponent{
-    _id = ''
-    constructor(id) {
-       this.id = id;
+export class CardAdocaoComponent extends HTMLElement{
+  
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' }); 
+       
     }
 
-    criar(id, foto, nome, local, statusAdocao){
-         const wrapper = document.createElement('card-adocao-wrapper');
+    static get observedAttributes() {
+        return ['codigo', 'foto', 'nome', 'local', 'adotado'];
+    }
 
-        wrapper.innerHTML = `
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render(){
+         
+       const codigo = this.getAttribute('codigo') || '';
+       const foto = this.getAttribute('foto') || '';
+       const nome = this.getAttribute('nome') || '';
+       const local = this.getAttribute('local') || '';
+       const adotado = this.getAttribute('adotado') || '';
+
+         this.shadowRoot.innerHTML = `
             <style>
-                .card-animal > a > div{
-                    position: relative; 
-                    max-width: 300px;
-                    aspect-ratio: 1 / 1;
-                    background-color: #fff;
-                    border-radius: 8px;
-                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                    text-align: center;
-                    transition: transform 0.3s ease, box-shadow 0.3s ease;
-                    cursor: pointer;
+                .card-adocao {
+                    
+                    display: grid;
+                    gap: 10px;
+                    gridTemplateColumns: repeat( auto-fit, minmax(max(200px, 20%), 1fr));
+                    padding: 10px;
 
-                    > img {
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                        display: block;
-                        border-radius: 8px 8px 0 0;
-                    }
+                    text-decoration: none;
+                    font-family: 'Jockey One';
 
-                    .tag-adotado{
-                        height: 22px;
-                        position: absolute;
-                        width: 100%;
-                        background-color: rgb(255, 172, 12);
-                        color: white;
-                        margin-top: -22px;
-                    }
+                    > div > a > div{
+                        position: relative; 
+                        max-width: 300px;
+                        aspect-ratio: 1 / 1;
+                        background-color: #fff;
+                        border-radius: 8px;
+                        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                        text-align: center;
+                        transition: transform 0.3s ease, box-shadow 0.3s ease;
+                        cursor: pointer;
 
-                    .card-nome {
-                        color: #27A8AD;
-                        font-size: 25px;
-                        font-weight: 700;
-                        line-height: 1.25;              /* compacta, favorece 2 linhas */
-                        -webkit-line-clamp: 2;          /* limita a 2 linhas */
-                        padding-bottom: 10px;
-                    }
+                        > img {
+                            width: 100%;
+                            height: 100%;
+                            object-fit: cover;
+                            display: block;
+                            border-radius: 8px 8px 0 0;
+                        }
 
-                    .card-local {
-                        color: black;
-                        font-family: Arial;
-                        font-weight: 400;
-                        font-size: 18px;
-                        line-height: 1.35;
-                        -webkit-line-clamp: 2;          /* limita a 2 linhas */
-                        padding-bottom: 10px;
+                        .tag-adotado{
+                            height: 22px;
+                            position: absolute;
+                            width: 100%;
+                            background-color: rgb(255, 172, 12);
+                            color: white;
+                            margin-top: -22px;
+                            padding: 5px 0px 5px 0px;
+                        }
+
+                        .card-nome {
+                            color: #27A8AD;
+                            font-size: 25px;
+                            font-weight: 700;
+                            line-height: 1.25;              
+                            -webkit-line-clamp: 2;         
+                            padding-bottom: 10px;
+                            
+                        }
+
+                        .card-local {
+                            color: black;
+                            font-family: Arial;
+                            font-weight: 400;
+                            font-size: 18px;
+                            line-height: 1.35;
+                            -webkit-line-clamp: 2;          /* limita a 2 linhas */
+                            padding-bottom: 10px;
+                        }
                     }
-                } 
-                .card-animal > a > div:hover {
-                transform: scale(1.05); /* aumenta 5% */
-                box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15); /* sombra mais forte */
+                }
+
+                .card-adocao > div > a > div:hover {
+                transform: scale(1.05);
+                box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
+                }
+
+                .card-adocao > div > a{
+                
+                text-decoration: none;
                 }
 
             </style>
-            <div class="card-animal">
-                <a href="../detalhes-adocao/index.html?id=${id}">
+            
+        <div class="card-adocao">
+        
+            <div>
+                <a href="../detalhes-adocao/index.html?id=${codigo}">
                     <div>
                         <img src="${foto}" alt="Foto do Animal">
                         
-                        ${statusAdocao ? `
+                        ${adotado === 'true' ? `
                         <div class="tag-adotado">
-                            <p>Adotado</p>
+                            ADOTADO
                         </div>
                         ` : ''}
 
@@ -78,20 +116,13 @@ export class CardAdocaoComponent{
                     </div>
                 </a>
             </div>
+        </div>
         `;
 
-        const idElemento = document.getElementById(this.id)
-   
-        idElemento.appendChild(wrapper);
-
-        idElemento.style.display = "grid";
-        idElemento.style.gap = "10px";
-        idElemento.style.gridTemplateColumns = "repeat( auto-fit, minmax(max(200px, 20%), 1fr))";
-        idElemento.style.padding = "10px";
     }
-    limpar() {
-        document.getElementById(this.id).innerHTML = '';
-    }
+    
 
 }
+
+customElements.define('card-adocao-component', CardAdocaoComponent);
 

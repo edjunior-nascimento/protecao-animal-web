aa85a6391877fd6457a4ba6e55bacfd79444c865
nilton99 <niltonalmeida1999#gmail.com>
2026-06-06 00:48:06 -0300

    "fix(adocao) posicionado carrocel e concluido a tela"

---
+++ react-app/src/components/features/CarrocelTelas/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  Pagination,
+  PaginationItem,
+  Stack,
+} from "@mui/material";
+
+export default function CustomPagination() {
+  const [page, setPage] = useState(4);
+
+  return (
+    <Stack spacing={2} alignItems="center" sx={{ p: 2 }}>
+      <Pagination
+        count={100}
+        page={page}
+        onChange={(e, value) => setPage(value)}
+        variant="outlined"
+        shape="rounded"
+        renderItem={(item) => (
+          <PaginationItem
+            {...item}
+            slots={{
+              previous: () => <span>{"<<"}</span>,
+              next: () => <span>{">>"}</span>,
+            }}
+          />
+        )}
+        sx={{
+          "& .MuiPaginationItem-root": {
+            color: "white",
+            borderColor: "teal",
+            cursor: "pointer",
+          },
+
+          "& .Mui-selected": {
+  backgroundColor: "cyan !important",
+  color: "white !important",
+  borderColor: "cyan",
+},
+
+"& .Mui-selected:hover": {
+  backgroundColor: "cyan !important",
+  color: "white !important",},
+        }}
+      />
+    </Stack>
+  );
+}
--- react-app/src/pages/AdocaoPage/index.tsx
+++ react-app/src/pages/AdocaoPage/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Box, Button, Container, Input, Typography } from "@mui/material";
 import { FilterAnimais } from "../../components/features/FilterAnimais";
 import { CardAnimal } from "../../components/features/CardAnimal";
+import CarouselMui from "../../components/features/CarrocelTelas";
+import PaginationCarousel from "../../components/features/CarrocelTelas";
+import PaginationRounded from "../../components/features/CarrocelTelas";
 
 export const AdocaoPage: React.FC = () => {
   return (
     <Container>
-      <Box sx={{paddingLeft: { xs: 2, md: "75px" }, paddingRight: { xs: 2, md: "75px" }, paddingTop: { xs: 4, md: "71px" }, paddingBottom: { xs: 4, md: 8 }}}>
+      <Box sx={{paddingLeft: { xs: 2, md: "51px" }, paddingRight: { xs: 2, md: "51px" }, paddingTop: { xs: 4, md: "71px" }, paddingBottom: { xs: 4, md: 8 }}}>
         <Box>
           <Box display="flex" alignItems="center" marginBottom= "28px">
             <a
@@ -26,7 +29,7 @@ export const AdocaoPage: React.FC = () => {
             Encontre seu novo amigo
           </Typography>
         </Box>
-        <Box sx={{ bgcolor: "white", marginRight: "31px", marginBottom: "79px", marginTop: "28px", display: "grid", gridTemplateColumns: { xs: "1fr", md: "repeat(3, 1fr)" }, gap: "20px", alignItems: "stretch", paddingLeft: "28px", paddingRight: "28px", paddingBottom: "34px", paddingTop: "35px", color: "blacka acade", borderRadius: "8px" }}>
+        <Box sx={{ bgcolor: "white", marginBottom: "79px", marginTop: "28px", display: "grid", gridTemplateColumns: { xs: "1fr", md: "repeat(3, 1fr)" }, gap: "20px", alignItems: "stretch", paddingLeft: "28px", paddingRight: "28px", paddingBottom: "34px", paddingTop: "35px", color: "blacka acade", borderRadius: "8px" }}>
               <Box sx={{ minWidth: 0, height: "56px" }}>
                 <FilterAnimais label="Todas as espécies" opcoes={[{ valor: 'cachorro', label: 'Cachorro' }, { valor: 'gato', label: 'Gato' }]} />
               </Box>
@@ -48,9 +51,12 @@ export const AdocaoPage: React.FC = () => {
                 </Button>
               </Box>
             </Box>
-            <Box sx={{ display: "grid", gridTemplateColumns: { xs: "4fr", md: "repeat(4, 1fr)" }, gap: "20px", alignItems: "stretch", paddingLeft: "28px", paddingRight: "28px", paddingBottom: "34px", paddingTop: "35px" }}>
+            <Box sx={{ display: "grid", gridTemplateColumns: { xs: "repeat(2, 1fr)", md: "repeat(4, 1fr)" }, gap: "20px", alignItems: "stretch", paddingBottom: "34px", paddingTop: "35px" }}>
               <CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={true} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://tse1.mm.bing.net/th/id/OIP.qUZKYeiway1yQS3pAev09wAAAA?r=0&cb=thfc1falcon&rs=1&pid=ImgDetMain&o=7&rm=3" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://i0.hippopx.com/photos/724/19/771/dog-labrador-pet-sitting-dog-thumb.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://i.ytimg.com/vi/RbYTtzubg8I/maxresdefault.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} /><CardAnimal nome="Rex" cidade="Centro" imagem="https://2.bp.blogspot.com/-07_Da0vxQuk/UPGaadF1VQI/AAAAAAAAADE/1KDhiQPNJa8/s320/Cachorro_Feliz.jpg" adotado={false} onClick={() => {}} />
 
+            </Box>
+            <Box sx={{ display: "flex", marginTop: "24px", justifyContent: "flex-end" }}>
+              <PaginationRounded />
             </Box>
       </Box>
     </Container>
